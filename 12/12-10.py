d33df561b6e07fd75d38884763ea9740eec84265
Ямалиева Карина Радисовна <104339306+krnamalieva@users.noreply.github.com>
2022-06-19 01:07:25 +0500

    Урок 12.

---
+++ 12/12-10.py
@@ -0,0 +1,6 @@
+line = input().split()
+result = 0
+for word in line:
+    result += len(word)
+print(result)
+    
---
+++ 12/12-12.py
@@ -0,0 +1,6 @@
+line = input().lower()
+result = list()
+for char in set(line):
+    result.append(line.count(char))
+print(max(result))
+
