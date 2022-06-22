bfa6401afbd8b5e044e4086fcb1b43900db2186d
Ямалиева Карина Радисовна <104339306+krnamalieva@users.noreply.github.com>
2022-06-22 10:37:02 +0500

    Урок 03.

---
+++ 03/03-01.py
@@ -0,0 +1,8 @@
+temperature = float(input("Введите температуру: "))
+if temperature < 15.5:
+    print("ХОЛОДНО")
+elif temperature > 28:
+    print("ЖАРКО")
+else:
+    print("НОРМАЛЬНО")
+    
---
+++ 03/03-04.py
@@ -0,0 +1,5 @@
+a = input()
+while len(a) > 0:
+    print(a)
+    a = input()
+    
